ed5bf6394091537935f4a2ffb3fd9aace6d965a9
Sho Iizuka <sho.i518@gmail.com>
2020-07-25 14:46:55 +0000

    Fix test

--- test/test.sh
+++ test/test.sh
@@ -2,5 +2,5 @@
 set -e
 ORIGIN="http://mimetex"
 # x=\frac{-b\pm\sqrt{b^2-4ac}}{2a}
-curl -fsSL -o test.gif "${ORIGIN}/cgi-bin/mimetex.cgi?x%3D%5Cfrac%7B-b%5Cpm%5Csqrt%7Bb%5E2-4ac%7D%7D%7B2a%7D"
-cmp -s quadratic.gif test.gif
+curl -fsSL -o /tmp/test.gif "${ORIGIN}/cgi-bin/mimetex.cgi?x%3D%5Cfrac%7B-b%5Cpm%5Csqrt%7Bb%5E2-4ac%7D%7D%7B2a%7D"
+cmp -s quadratic.gif /tmp/test.gif
