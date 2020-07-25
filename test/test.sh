aaf9e1880ca2a0d51aa330c385389cc594d61c9b
Sho Iizuka <sho.i518@gmail.com>
2020-07-25 15:19:48 +0000

    Revert "Update test.sh"
    This reverts commit 1447e60b653161c166a3a11059090ecd5cf1e9c8.

--- test/test.sh
+++ test/test.sh
@@ -2,5 +2,5 @@
 set -e
 ORIGIN="http://mimetex"
 # x=\frac{-b\pm\sqrt{b^2-4ac}}{2a}
-curl -fsSL -o /tmp/test.gif "${ORIGIN}/cgi-bin/mimetex.cgi?y%3D%5Cfrac%7B-b%5Cpm%5Csqrt%7Bb%5E2-4ac%7D%7D%7B2a%7D"
+curl -fsSL -o /tmp/test.gif "${ORIGIN}/cgi-bin/mimetex.cgi?x%3D%5Cfrac%7B-b%5Cpm%5Csqrt%7Bb%5E2-4ac%7D%7D%7B2a%7D"
 cmp -s quadratic.gif /tmp/test.gif
